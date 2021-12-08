6e37b0d3c33b9d84cbc84bb729a7b1df457cb8b9
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-12-08 23:20:32 +0530

    3.4 : usage of npm node packages

--- note-app/app.js
+++ note-app/app.js
@@ -1,5 +1,12 @@
 const addNum = require('./utils.js')
 const notes = require("./notes.js")
+const validator = require('validator')
+
+
+const result1 = validator.isEmail("vinod@gmail.com")
+const result2 = validator.isURL("https//abc.com")
+console.log(result1)
+console.log(result2)
 
 const sum = addNum(2, 4)
 const myNote = notes()
