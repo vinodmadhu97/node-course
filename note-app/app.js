3a1880bb9f9cfac5fa6d3aa32ce56f721caed136
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-12-09 23:11:35 +0530

    4.3 : command line argument with yargs npm plugin

--- note-app/app.js
+++ note-app/app.js
@@ -1,17 +1,23 @@
 const addNum = require('./utils.js')
 const notes = require("./notes.js")
 const validator = require('validator')
+const yargs = require('yargs')
+const { command, describe } = require('yargs')
 
 
 const arg1 = process.argv
-const arg2 = process.argv[2]
+const arg2 = yargs.argv
 console.log(arg1)
 console.log(arg2)
 
-if(arg2 === "add"){
-    console.log("add something")
-}else{
-    console.log("remove")
-}
+yargs.version("1.1.0")
+yargs.command({
+    command: "add",
+    describe : "adding new note",
+    handler : function(){
+        console.log("new file is added")
+    }
+})
+
 
 
