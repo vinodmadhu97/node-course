1160714f35937bc258eb8c64c9cb762858d68f7e
vinodmadhu97 <vinodmadhu97@gmail.com>
2021-12-09 22:37:59 +0530

    4.2: command line argument

--- note-app/app.js
+++ note-app/app.js
@@ -3,14 +3,15 @@ const notes = require("./notes.js")
 const validator = require('validator')
 
 
-const result1 = validator.isEmail("vinod@gmail.com")
-const result2 = validator.isURL("https://abc.com")
-console.log(result1)
-console.log(result2)
+const arg1 = process.argv
+const arg2 = process.argv[2]
+console.log(arg1)
+console.log(arg2)
 
-const sum = addNum(2, 4)
-const myNote = notes()
-console.log(sum)
-console.log(myNote)
+if(arg2 === "add"){
+    console.log("add something")
+}else{
+    console.log("remove")
+}
 
 
